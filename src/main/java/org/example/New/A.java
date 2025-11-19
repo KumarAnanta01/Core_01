95dc9d57c53a967f97bd223d91cd7371d14a296f
KumarAnanta01 <Ananta.Kumar9096@gmail.com>
2025-11-19 14:03:39 +0530

    new file is added

---
+++ src/main/java/org/example/New/A.java
@@ -0,0 +1,27 @@
+package org.example.New;
+
+import java.util.Scanner;
+
+public class A {
+
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        Scanner Sc= new Scanner(System.in);
+
+        System.out.println("Enter two numbers");
+
+        a=Sc.nextInt();
+        b=Sc.nextInt();
+
+        c=a+b;
+
+        System.out.println("the result of the number is "+c);
+
+
+
+
+    }
+}
